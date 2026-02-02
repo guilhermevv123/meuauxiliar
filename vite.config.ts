e2209111996a89d1874ff849e4237f62519fad82
guilhermevv123 <guilhermevv123@users.noreply.github.com>
2026-02-02 17:45:48 -0300

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/meuauxiliar/' : '/',
+  base: "/",
   server: {
     host: "::",
     port: 8080,
